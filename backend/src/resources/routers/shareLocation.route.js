6d7a1541f0ed9fd0de607cca70a34da83f97337f
tomaugust03 <nguyenminhtien3820@gmail.com>
2022-12-27 08:16:24 +0700

    Fix: Suggest A Place

--- backend/src/resources/routers/shareLocation.route.js
+++ backend/src/resources/routers/shareLocation.route.js
@@ -3,7 +3,11 @@ const route = express.Router()
 const controller = require('../../app/controllers/shareLocation.controller')
 const authMiddleware = require('../middleware/auth.middleware')
 
-route.post('', authMiddleware.isStaff, controller.createShareLocation)
+route.post(
+  '',
+  authMiddleware.isOptionLogin,
+  controller.createShareLocation
+)
 route.get('', authMiddleware.isStaff, controller.getListShareLocation)
 route.put(
   '/update',
